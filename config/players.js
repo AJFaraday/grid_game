b3da74f092eaa38b6a1e20b77ca633f828ac8c63
Andrew Faraday <ajfaraday@edge-ka.com>
2018-08-31 21:35:28 +0100

    Bots work!!!

--- config/players.js
+++ config/players.js
@@ -3,33 +3,21 @@ GridGame.data.players = [
     name: 'green',
     start: GridGame.positions.north_west_corner(2),
     direction: 'east',
-    playable: true
+    playable: true,
+    towardsX: 'east',
+    towardsY: 'south',
+    awayX: 'west',
+    awayY: 'north'
   },
   {
     name: 'red',
     start: GridGame.positions.south_east_corner(2),
     direction: 'west',
-    playable: true
+    playable: true,
+    towardsX: 'west',
+    towardsY: 'north',
+    awayX: 'east',
+    awayY: 'south'
   }
-  /*
-  ,
-  {
-    name: 'yellow',
-    start: {
-      x: 8,
-      y: 0
-    },
-    direction: 'south',
-    playable: false
-  },
-  {
-    name: 'blue',
-    start: {
-      x: 11,
-      y: 11
-    },
-    direction: 'north',
-    playable: false
-  }
-  */
+  // Only 2 for API-driven game
 ];
