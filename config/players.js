b5686f78ccf5017851545bef19fe5a824f994888
Andrew Faraday <ajfaraday@edge-ka.com>
2019-09-25 20:58:18 +0100

    API UI tidied up, gist cache holds some locally

--- config/players.js
+++ config/players.js
@@ -1,23 +1,29 @@
-GridGame.data.players = [
-  {
-    name: 'green',
-    start: GridGame.positions.north_west_corner(2),
-    direction: 'east',
-    playable: true,
-    towardsX: 'east',
-    towardsY: 'south',
-    awayX: 'west',
-    awayY: 'north'
-  },
-  {
-    name: 'red',
-    start: GridGame.positions.south_east_corner(2),
-    direction: 'west',
-    playable: true,
-    towardsX: 'west',
-    towardsY: 'north',
-    awayX: 'east',
-    awayY: 'south'
+Players = {
+  init: function() {
+    GridGame.data.players = [
+      {
+        name: 'green',
+        start: GridGame.positions.north_west_corner(2),
+        direction: 'east',
+        playable: true,
+        towardsX: 'east',
+        towardsY: 'south',
+        awayX: 'west',
+        awayY: 'north'
+      },
+      {
+        name: 'red',
+        start: GridGame.positions.south_east_corner(2),
+        direction: 'west',
+        playable: true,
+        towardsX: 'west',
+        towardsY: 'north',
+        awayX: 'east',
+        awayY: 'south'
+      }
+      // Only 2 for API-driven game
+    ];
   }
-  // Only 2 for API-driven game
-];
+};
+
+Players.init();
