93f4b90fa6cb90e178851bfdba444681f07c7ac1
andrew james faraday <ajfaraday@batman.alces.co.uk>
2019-10-23 12:37:15 +0100

    Added consented bots, rearranged select and tidied styles

--- config/bots.js
+++ config/bots.js
@@ -1,11 +1,15 @@
 GridGame.data.bots = {
-  'All Towards': '808c86eb544d9efaf94a30efc91bdfaf',
-  'Custom': 'code',
-  'Faraday Cage': 'd9eb9a70ad0dc0fb1885be0fce032adc',
-  'Gist Id': 'bot_id',
-  'Long Random': 'b9a0fe99060d9be1fc617bb7262f57be',
-  'Manual': 'manual',
-  'Rotate': '6634a7f381bb679ee7c2d90d93588d66',
-  'Short Random': 'b16df33acb0f0c7830d2ca2d656f80be'
+  'Basic - All Towards': '808c86eb544d9efaf94a30efc91bdfaf',
+  'Basic - Faraday Cage': 'd9eb9a70ad0dc0fb1885be0fce032adc',
+  'Basic - Long Random': 'b9a0fe99060d9be1fc617bb7262f57be',
+  'Basic - Rotate': '6634a7f381bb679ee7c2d90d93588d66',
+  'Basic - Short Random': 'b16df33acb0f0c7830d2ca2d656f80be',
+  'Special - Custom': 'code',
+  'Special - Gist Id': 'bot_id',
+  'Special - Manual': 'manual',
+  'Alion - Doombeam': '496367ff51c27dc62ebdc473cd14f333',
+  'BradC - Double Barrel': '7d0a88c1533fc89b2ee26484bbcafc59',
+  'BradC - Sweeping Endgame': '3f69bdf2be37886ff396d0c485007e80',
+  'BradC - Quad Rush': 'a0a4a9f3c8b8863392f109fdfd52af00'
 };
 
